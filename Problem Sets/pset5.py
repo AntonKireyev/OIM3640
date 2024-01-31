32df231b81972d21af1f0a341b61da2978dec723
AntonKireyev <122702314+AntonKireyev@users.noreply.github.com>
2024-01-31 14:46:29 -0500

    Update pset5.py

--- Problem Sets/pset5.py
+++ Problem Sets/pset5.py
@@ -1,66 +1,76 @@
 ''' Dice Rolling Simulator
 
-This problem makes use of the random module, either a for loop or while loop and if...elif...else. 
-Try entering import random into the shell, then explore the functions within the module. 
-For this activity you will need to simulate rolling dice.  
-Can you figure out a way to “randomly roll” a single 6-sided die? 
-Now write a program that simulates rolling two dice and keeps track of the total of each roll, 
+A program that simulates rolling two dice and keeps track of the total of each roll, 
 and how many times in the simulation that total was "rolled".  
 The program will then output each possible roll total and the number of times it occurred as a pipe or other symbol.  
-Allow a user to specify how many rolls to simulate.  
+Allows a user to specify how many rolls to simulate.  
 '''
-
+import math
 import random
 
-rolls = int(input("how many rolls: "))
-dice = [1,2,3,4,5,6]
-results =[]
-'''
-two = 0
-three = 0
-four = 0
-five = 0
-six = 0
-seven = 0
-eight = 0
-nine = 0
-ten = 0
-eleven = 0
-twelve = 0
-'''
+intro = "Welcome to the dice rolling simulator\nGet ready to roll dem bones!\nHow many rolls should we simulate?\n\n"
 
-# print # of dice rolls defined by user
+user_input = input(intro)
+
+rolls = math.floor(float(user_input))
+dice = [1,2,3,4,5,6]
+
+COUNTER = "*"
+
+Twos = ""
+Threes = ""
+Fours = ""
+Fives = ""
+Sixes = ""
+Sevens = ""
+Eights = ""
+Nines = ""
+Tens = ""
+Elevens = ""
+Twelves = ""
+
+# simulate # of dice rolls defined by user and adjust COUNTERs
 for iteration in range(rolls):
     d1 = random.choice(dice)
     d2 = random.choice(dice)
     dsum = d1 + d2
-    print(f"{iteration:<10}{d1} + {d2} = {dsum}")
-    results.append(dsum)
+    if dsum == 2:
+        Twos += COUNTER
+    elif dsum == 3:
+        Threes += COUNTER
+    elif dsum == 4:
+        Fours += COUNTER
+    elif dsum == 5:
+        Fives += COUNTER
+    elif dsum == 6:
+        Sixes += COUNTER
+    elif dsum == 7:
+        Sevens += COUNTER
+    elif dsum == 8:
+        Eights += COUNTER
+    elif dsum == 9:
+        Nines += COUNTER
+    elif dsum == 10:
+        Tens += COUNTER
+    elif dsum == 11:
+        Elevens += COUNTER
+    elif dsum == 12:
+        Twelves += COUNTER
+    else:
+        print("Unexpected Error")
+        continue
 
-print(results)
-
-if 2 in results:
-    print("snake eyes!")
-elif 3 in results:
-    pass
-elif 4 in results:
-    pass
-elif 5 in results:
-    pass
-elif 6 in results:
-    pass
-elif 7 in results:
-    pass
-elif 8 in results:
-    pass
-elif 9 in results:
-    pass
-elif 10 in results:
-    pass
-elif 11 in results:
-    pass
-elif 12 in results:
-    pass
-
-    
-
+# Print Outputs
+print(f"Simulation outcome:")
+print('-' * 20)
+print(f"{'Twos':8}: {Twos}")
+print(f"{'Threes':8}: {Threes}")
+print(f"{'Fours':8}: {Fours}")
+print(f"{'Fives':8}: {Fives}")
+print(f"{'Sixes':8}: {Sixes}")
+print(f"{'Sevens':8}: {Sevens}")
+print(f"{'Eights':8}: {Eights}")
+print(f"{'Nines':8}: {Nines}")
+print(f"{'Tens':8}: {Tens}")
+print(f"{'Elevens':8}: {Elevens}")
+print(f"{'Twelves':8}: {Twelves}")
