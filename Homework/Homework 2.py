ef4cf1e9610406bfbe95387a16a7156585f6579e
AntonKireyev <akireyev1@babson.edu>
2024-02-18 19:06:04 -0500

    Update Homework 2.py

--- Homework/Homework 2.py
+++ Homework/Homework 2.py
@@ -2,67 +2,76 @@
 
 # Import Libraries
 import numpy_financial as npf
+import math
 
 # Variables / Inputs
-i_total = 0
-p_total = 0
+i_total = list()
+p_total = list()
 DIV = "-" * 54
 
 # Sample / manual selector
 print("Welcome to the Loan Amortizer \n Please enter S for sample report or C to enter new parameters")
 mode = input("[S]ample or [C]ustom Input: ")       # Mode Selector
 while mode not in "SCsc":
-    mode = input("Error. Type [S] or [C]:  ")
+    mode = input("Error! Type [S] or [C]:  ")
 
 timescale = input("[M]onthly or [A]nnual Report: ")        # Annual/Monthly Selector
 while timescale not in "AMam":
-    timescale = input("Error. Type [M] or [A]:  ")
+    timescale = input("Error! Type [M] or [A]:  ")
 
 print(DIV)
 
 # Sample/Custom Mode Selector
 if mode in "Ss":
     balance = 360000        # In Dollars
-    loan_time = 360         # In Months
-    int_percent = 5       # Annual Rate in %
-    monthly_pmnt = 2129.54    # Monthly payment in Dollars
-    intro = f"{'Balance:':<28}${balance:>15,.2f}\n{'Length of Loan in Months:':<28}{loan_time:>15,}\n{'Annual interest Rate:':<28}{int_percent:>15,.2f}%\n{'Monthly Payment:':<28}${monthly_pmnt:>15,.2f}"
-    print(intro)
+    loan_time = 10         # In Years
+    int_percent = 5.00       # Annual Rate in %
+    monthly_pmnt = npf.pmt(int_percent/1200, loan_time, -balance)   # Monthly payment in Dollars
 
 if mode in "Cc":
-    balance = float(input("Balance: $"))
-    loan_time = float(input("Loan length in years: ")*12)
+    try:
+        balance = float(input("Loan Amount: $"))
+        while balance <= 0:
+            balance = float(input("Error! Please enter valid loan amount: "))
+            break
+    except:
+        print("Invalid Input")
+    loan_time = float(input("Loan length in years (between 10-40): "))
+    while loan_time < 10 or loan_time > 40:
+        loan_time = float(input("Error! Please enter a number of years between 10 and 40: "))
     int_percent = float(input("Annual Interest Rate: "))
-    monthly_pmnt = float(input("Monthly Payment: $"))
+    while int_percent <= 0:
+        int_percent = float(input("Error! Please enter a positive annual interest rate: "))
+    monthly_pmnt = npf.pmt(int_percent/1200, loan_time, -balance)   # Monthly payment in Dollars
 
+intro = f"{'Balance:':<28}${balance:>15,.2f}\n{'Length of Loan in Months:':<28}{loan_time:>16,.0f}\n{'Annual interest Rate:':<28}{int_percent:>15,.2f}%\n{'Monthly Payment:':<28}${monthly_pmnt:>15,.2f}"
+print(intro)
 print(DIV)
 
 # Annual/Monthly Period Selector
 if timescale in "Mm":
     int_rate = int_percent / 1200
-    time = loan_time
+    term = loan_time * 12
     period_pmnt = monthly_pmnt
     time_desc = 'Months'
 
 if timescale in "Aa":
-    time = loan_time / 12
+    term = loan_time
     int_rate = int_percent / 100
     period_pmnt = monthly_pmnt * 12
     time_desc = 'Years'
 
 # Payment Amounts
-header = f"{time_desc:<10}{'Principal':<15}{'Interest':<15}{'Payment':<15}{'Remaining Balance':<15}"
+header = f"{time_desc:<10}{'Principal':<15}{'Interest':<15}{'Payment':<15}{'Total Principal Paid':<15}"
 print(header)
 print(DIV)
 
-time = int(time)
-
-for period in (range(1, time)):
+for period in (range(1, int(term)+1)):
     # Payments
-    i_payment = abs(npf.ipmt(int_rate, period, time, balance))
-    p_payment = period_pmnt - i_payment
-    i_total += i_payment
-    p_total += p_payment
+    i_payment = npf.ipmt(int_rate, period, term, -balance)
+    p_payment = npf.ppmt(int_rate, period, term, -balance)
+    i_total.append(i_payment)
+    p_total.append(p_payment)
 
     if timescale in "Aa":
         if period % 10 == 1 and period > 1:      # introduce a line break after every 10 years.
@@ -72,20 +81,19 @@ for period in (range(1, time)):
         if period % 12 == 1:
             print(f"{'-'*20}{'Year: '}{int((period/12)+1)}{'-'*20}")
 
-    if balance <= p_payment:
-        p_payment = balance
-        period_pmnt = p_payment + i_payment
-
     # Calculate Remaining balance
-    balance -= p_payment
-    print(f"{period:<10}{p_payment:<15,.2f}{i_payment:<15,.2f}{period_pmnt:<15,.2f}{balance:<15,.2f}")
+    print(f"{period:<10}{p_payment:<15,.2f}{i_payment:<15,.2f}{p_payment + i_payment:<15,.2f}{sum(p_total):<15,.2f}")
 
     if balance <= 0:
         break
 # Footer
-footer = f"{'Total:':<9}$ {p_total:<13,.2f}$ {i_total:<13,.2f}$ {i_total + p_total:<13,.2f}"
+footer = f"{'Total:':<9}$ {sum(p_total):<13,.2f}$ {sum(i_total):<13,.2f}$ {sum(i_total) + sum(p_total):<13,.2f}"
 print(DIV)
 print(footer)
 
-half_life = (period / 2) * 12
-print(half_life)
+paid = 0
+for index in range(1, len(p_total)+1):
+    paid += p_total[index]
+    if paid >= math.ceil(balance/2):
+        print(f"\nThe principal is half paid off after {index:.0f} {time_desc.lower()}!")
+        break
