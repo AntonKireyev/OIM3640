04f19bf586991dbbc826a80523303e0a63165d5c
AntonKireyev <122702314+AntonKireyev@users.noreply.github.com>
2024-03-26 17:06:12 -0400

    :)

--- Homework/stock_tracker_functions.py
+++ Homework/stock_tracker_functions.py
@@ -3,10 +3,36 @@ from time import sleep, time
 import sys
 import yfinance as yf
 
+"""
+FUNCTIONS NEEDED:
+    1. Track Watchlist
+    2. Add Watchlist (WIP)
+    3. Edit Watchlist (WIP)
+    4. Delete Watchlist (WIP)
+    5. Exit
+
+"""
+
+# read_directory function, will need to use it to view a directory.
+
+def read_directory(): 
+      if not os.path.exists('watchlists'):            # test for existence of folder
+            print("No watchlist directory, creating")
+            os.mkdir("watchlists")
+      else:
+            watchlists = os.listdir('watchlists')                        # returns list with names of files
+            if not watchlists:
+                  print("No saved list")
+            else:
+                  for count, watchlist in enumerate(watchlists, 1):                    # Print numbered list of files.
+                        watchlist = watchlist[:-4]
+                        print(f'{count}.  {watchlist}')
+                  return watchlists
+            
+
 # Track list
 '''
 1. Allow continous tracking of an N list of stocks
-2. allow user to choose which list
 '''
 
 watchlist = 'AMZN NFLX META GOOG'.split()
@@ -27,23 +53,9 @@ def track(watchlist):
 
 
 # Choose Watchlist
-                
 '''
-1. 
+2. allow user to choose which list
 '''
-def read_directory(): 
-      if not os.path.exists('watchlists'):            # test for existence of folder
-            print("No watchlist directory, creating")
-            os.mkdir("watchlists")
-      else:
-            watchlists = os.listdir('watchlists')                        # returns list with names of files
-            if not watchlists:
-                  print("No saved list")
-            else:
-                  for count, watchlist in enumerate(watchlists, 1):                    # Print numbered list of files.
-                        watchlist = watchlist[:-4]
-                        print(f'{count}.  {watchlist}')
-                  return watchlists
 def choose_list():
       lists = read_directory()
       choice = int(input("Pick a watchlist, type the number: "))
@@ -56,3 +68,24 @@ def choose_list():
 
 
 
+# Add Watchlist
+
+'''
+1.
+'''
+
+def add_watchlist():
+     pass   # TODO
+
+
+# Edit Watchlists
+
+def edit_watchlist():
+     lists = read_directory()
+     pass   # TODO
+
+
+# Delete Watchlists
+
+def del_watchlist():
+     pass   # TODO
